4100e14310ddb7b65ea90ef474cb5847de2a9dc2
Kinvi Ekoue-Bla <KinviE@Magenic.com>
2018-08-22 21:16:28 -0400

    Add project files.

---
+++ RazorPagesCore21/Data/RazorPagesCore21Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesCore21.Models
+{
+    public class RazorPagesCore21Context : DbContext
+    {
+        public RazorPagesCore21Context (DbContextOptions<RazorPagesCore21Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesCore21.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorPagesCore21/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPagesCore21.Models;
+
+namespace RazorPagesCore21.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPagesCore21.Models.RazorPagesCore21Context _context;
+
+        public CreateModel(RazorPagesCore21.Models.RazorPagesCore21Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
